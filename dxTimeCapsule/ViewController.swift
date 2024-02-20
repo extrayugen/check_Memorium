60b84379ebb24cd74e30ae54cff073e1a8aa3c3c
Jooyeong(Yugen) <supershicker@gmail.com>
2024-02-20 17:31:55 +0900

    Initial Commit

---
+++ dxTimeCapsule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dxTimeCapsule
+//
+//  Created by t2023-m0031 on 2/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
