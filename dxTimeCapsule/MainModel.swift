29726b0dc59d8bf297dcd8a5fdc96af8be850f9c
Jooyeong(Yugen) <supershicker@gmail.com>
2024-02-21 11:25:41 +0900

    [Merge] None

--- dxTimeCapsule/MainModel.swift
+++ dxTimeCapsule/MainModel.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-// ViewModel: ViewController의 데이터 관리 및 비즈니스 로직 담당
 class MainModel {
     var welcomeMessage: String {
         return "Welcome to \n MainView"
---
+++ dxTimeCapsule/TimeCapsuleView.swift
@@ -0,0 +1,8 @@
+//
+//  TimeCapsuleView.swift
+//  dxTimeCapsule
+//
+//  Created by t2023-m0031 on 2/21/24.
+//
+
+import Foundation
