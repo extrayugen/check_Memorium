341da46082c15dd98e1745482b9a5ce7b03e9124
Owen Kim <godkwk5@naver.com>
2024-02-24 22:36:43 +0900

    [Style] rootVC초기값으로

--- dxTimeCapsule/SceneDelegate.swift
+++ dxTimeCapsule/SceneDelegate.swift
@@ -24,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         self.window = window
         
-        let rootVC = MainCapsuleViewController()
+        let rootVC = AuthenticationViewController()
         
         // 스플래시 화면 2초 노출
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
